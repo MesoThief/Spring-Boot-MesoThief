19a03ccf0e24a9f7b5ab89ab391205d35da31ae3
MesoThief <pckyu01@naver.com>
2022-07-10 22:42:49 +0900

    RestClientTest 작성

--- src/test/kotlin/com/havi/bookRestTest/BookRestTest.kt
+++ src/test/kotlin/com/havi/bookRestTest/BookRestTest.kt
@@ -4,6 +4,7 @@ import com.havi.service.BookRestService
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Rule
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.junit.rules.ExpectedException
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -15,6 +16,8 @@ import org.springframework.test.web.client.MockRestServiceServer
 import org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo
 import org.springframework.test.web.client.response.MockRestResponseCreators.withServerError
 import org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess
+import org.springframework.web.client.HttpServerErrorException
+import org.springframework.web.client.HttpServerErrorException.InternalServerError
 
 @RunWith(SpringRunner::class)
 @RestClientTest(BookRestService::class)
@@ -30,12 +33,20 @@ class BookRestTest {
 
     @Test
     fun restTest() {
-        // FIXME
+        server.expect(requestTo("/rest/test"))
+            .andRespond(withSuccess(ClassPathResource("/test.json", javaClass), MediaType.APPLICATION_JSON))
+        val book = bookRestService.getRestBook()
+        assertThat(book.title).isEqualTo("테스트")
     }
 
     @Test
     fun restErrorTest() {
-        // FIXME
-        // Note: this might not work
+        server.expect(requestTo("/rest/test"))
+            .andRespond(withServerError())
+        // thrown.expect(InternalServerError::class.java)
+        // bookRestService.getRestBook()
+
+        assertThrows<HttpServerErrorException> { bookRestService.getRestBook() }
+        // 성...공?
     }
 }

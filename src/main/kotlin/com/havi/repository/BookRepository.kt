80cc16a4765c8bc3fa8ddb08bc533b88d50652f1
MesoThief <pckyu01@naver.com>
2022-07-16 16:37:43 +0900

    다른 챕터 코드 삭제

--- src/main/kotlin/com/havi/repository/BookRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.havi.repository
-
-import com.havi.domain.Book
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface BookRepository : JpaRepository<Book, Int>
--- src/main/kotlin/com/havi/service/BookService.kt
+++
@@ -1,7 +0,0 @@
-package com.havi.service
-
-import com.havi.domain.Book
-
-interface BookService {
-    fun getBookList(): List<Book>
-}

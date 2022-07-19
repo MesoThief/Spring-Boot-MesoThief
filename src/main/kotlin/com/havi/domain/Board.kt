9ac6e3df19874056cfe41db042447888845a28e5
MesoThief <pckyu01@naver.com>
2022-07-19 23:13:09 +0900

    Update Board.kt

--- src/main/kotlin/com/havi/domain/Board.kt
+++ src/main/kotlin/com/havi/domain/Board.kt
@@ -1,8 +1,10 @@
 package com.havi.domain
 
 import com.havi.domain.enums.BoardType
-import java.time.LocalDateTime
+import javax.persistence.Column
 import javax.persistence.Entity
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
 import javax.persistence.FetchType
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
@@ -12,17 +14,24 @@ import javax.persistence.Table
 
 @Entity
 @Table
-class Board {
+class Board(
+    @Column
+    var title: String,
+
+    @Column
+    var subTitle: String,
+
+    @Column
+    var content: String,
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    var boardType: BoardType,
+
+    @OneToOne(fetch = FetchType.LAZY)
+    var user: User,
+): AuditLoggingBase(), java.io.Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var idx: Long = 0
-
-    lateinit var title: String
-    lateinit var subTitle: String
-    lateinit var content: String
-    lateinit var boardType: BoardType
-    lateinit var createdDate: LocalDateTime
-    lateinit var updatedDate: LocalDateTime
-    @OneToOne(fetch = FetchType.LAZY)
-    lateinit var user: User
+    var idx: Long? = null
 }
